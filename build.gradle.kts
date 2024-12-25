cbb9c007e8f71800139c2561de1ca46a44df0a91
りんご飴196 <kazumac41@gmail.com>
2024-12-25 13:36:45 +0900

    ✨  :slf4j-simple 追加

--- build.gradle.kts
+++ build.gradle.kts
@@ -19,6 +19,7 @@ dependencies {
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.6.4")
     val sqliteVersion = "3.46.1.0"
     implementation("org.xerial:sqlite-jdbc:$sqliteVersion") // 最新バージョンを指定してください
+    implementation("org.slf4j:slf4j-simple:2.0.9")
 }
 
 tasks.test {
