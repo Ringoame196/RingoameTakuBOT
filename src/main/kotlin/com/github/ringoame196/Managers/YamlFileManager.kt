1bb3ce8e30cc6464a214370a7434e8de6dd674c8
りんご飴196 <kazumac41@gmail.com>
2025-02-03 21:24:08 +0900

    ✨  :yamlファイルからデータ取得できるように

---
+++ src/main/kotlin/com/github/ringoame196/Managers/YamlFileManager.kt
@@ -0,0 +1,12 @@
+package com.github.ringoame196.Managers
+
+import org.yaml.snakeyaml.Yaml
+import java.io.File
+
+class YamlFileManager {
+    fun loadYAsMap(filePath: String): Map<String, String> {
+        val yaml = Yaml()
+        val inputStream = File(filePath).inputStream()
+        return yaml.load(inputStream) as Map<String, String>
+    }
+}
