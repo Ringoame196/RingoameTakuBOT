248f585bf49d6368815086607ae975d73d11d4d7
りんご飴196 <kazumac41@gmail.com>
2025-02-20 00:08:33 +0900

    🗑️  :削除

--- src/main/kotlin/com/github/ringoame196/datas/ScheduleData.kt
+++
@@ -1,9 +0,0 @@
-package com.github.ringoame196.datas
-
-data class ScheduleData(
-    val id:Int,
-    val scenarioName:String,
-    val datetime:String,
-    val channelId:String,
-    val status:Int
-)
