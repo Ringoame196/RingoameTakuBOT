7153cfc1fcb685e92a38fc9796875beec2fc8334
りんご飴196 <kazumac41@gmail.com>
2025-06-20 02:09:56 +0900

    ⬆️ makeHOコマンド エラー文出力 エラー対策

--- src/main/kotlin/com/github/ringoame196/slashCommand/MakeHOCommand.kt
+++ src/main/kotlin/com/github/ringoame196/slashCommand/MakeHOCommand.kt
@@ -8,12 +8,20 @@ class MakeHOCommand: Command {
 
     override fun runCommand(e: SlashCommandInteractionEvent) {
         val guild = e.guild ?: return
-        val scenarioName = discordManager.acquisitionCommandOptions(e,SlashCommandConst.SCENARIO_NAME_OPTION)?.asString ?: return
-        val hoNumber = discordManager.acquisitionCommandOptions(e,SlashCommandConst.HO_NUMBER_OPTION)?.asInt ?: return
+        val scenarioName = discordManager.acquisitionCommandOptions(e, SlashCommandConst.SCENARIO_NAME_OPTION)?.asString ?: return
+        val hoNumber = discordManager.acquisitionCommandOptions(e, SlashCommandConst.HO_NUMBER_OPTION)?.asInt ?: return
 
-        discordManager.makeHOChannel(guild,scenarioName,hoNumber)
+        discordManager.makeHOChannel(
+            guild,
+            scenarioName,
+            hoNumber,
+            onFailure = { error ->
+                e.reply("チャンネル作成中にエラーが発生しました: ${error.message}")
+                    .setEphemeral(true)
+                    .queue()
+            }
+        )
 
-        val message = "HOチャンネル作成完了しました"
-        e.reply(message).setEphemeral(true).queue()
+        e.reply("HOチャンネル作成を開始しました").setEphemeral(true).queue()
     }
 }
